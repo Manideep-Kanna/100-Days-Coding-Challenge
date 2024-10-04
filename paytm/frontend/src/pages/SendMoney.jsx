a27847050b54ec0650e779a6bc808245dfb87d03
Manideep-Kanna <manideepsitaram143@gmail.com>
2024-10-05 01:44:32 +0530

    Send Money Page is done

--- paytm/frontend/src/pages/SendMoney.jsx
+++ paytm/frontend/src/pages/SendMoney.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import HeadingComponent from '../components/HeadingComponent'
+import ProfileCircleComponent from '../components/ProfileCircleComponent'
+import InputBoxComponent from '../components/InputBoxComponent'
+import ButtonComponent from '../components/ButtonComponent'
 
 function SendMoney() {
   return (
-    <div>SendMoney</div>
+   <div className='mx-auto w-1/3 shadow-lg'>
+    <div className='flex justify-center mt-10 mb-24'>
+    <HeadingComponent text={"Send Money"} />
+    </div>
+    <div className='flex justify-start items-center gap-2'>
+      <ProfileCircleComponent text={"A"} />
+      <div className='font-bold text-xl'>Friend's Name</div>
+    </div>
+    <InputBoxComponent label={'Amount (in Rs)'} placeholder={"Enter amount"}/>
+    <ButtonComponent text={"Initiate Transfer"} onClick={"#"} />
+   </div>
   )
 }
 
